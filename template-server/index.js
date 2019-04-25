1a1729e2e98185f76c00df7866e89cb1c690fe57
Antoni Atanasov <antoni.atanasov@affinitas.de>
2019-04-25 22:33:53 +0200

    Add deletion handles

--- template-server/index.js
+++ template-server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const { listProjects, listTemplates } = require('./template-service');
+const {
+  removeProject,
+  listProjects,
+  listTemplates,
+  removeTemplate
+} = require('./template-service');
 
 const app = express()
 const port = 5000;
@@ -13,12 +18,23 @@ app.get('/', (req, res) => {
   res.send({ projects });
 });
 
+app.delete('/:projectName', (req, res) => {
+  const { projectName } = req.params;
+  removeProject(projectName);
+  res.send(`${projectName} was deleted`)
+});
+
 app.get('/:projectName', (req, res) => {
   const { projectName } = req.params
   const files = listTemplates(`${__dirname}/templates/${projectName}`);
   res.send(files);
 });
 
+app.delete('/:projectName/:templateName', (req, res) => {
+  const { projectName, templateName } = req.params;
+  removeTemplate(projectName, templateName);
+  res.send(`${templateName} was deleted`)
+});
 app.post('/:projectName/:templateName', (req, res) => {
   const { projectName, templateName } = req.params;
   if (!req.files || Object.keys(req.files).length === 0) {
--- template-server/template-service.js
+++ template-server/template-service.js
@@ -1,19 +1,52 @@
-const { lstatSync, readdirSync } = require('fs');
+const {
+  rmdirSync,
+  existsSync,
+  lstatSync,
+  readdirSync,
+  unlinkSync
+ } = require('fs');
 const { join } = require('path');
 
+function deleteFolderRecursive(path) {
+  if (existsSync(path)) {
+    readdirSync(path).forEach((file) => {
+      const curPath = `${path}/${file}`;
+      if (lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        unlinkSync(curPath);
+      }
+    });
+    rmdirSync(path);
+  }
+};
 const isDirectory = source => lstatSync(source).isDirectory();
 const isFile = source => lstatSync(source).isFile();
+
 function pathConverter(path) {
   const pathBlock = path.split("/")
   return pathBlock.pop();
 }
+
 function listProjects(path) {
   return readdirSync(path).map(name => join(path, name)).filter(isDirectory).map(pathConverter)
 }
+
 function listTemplates(path) {
   return readdirSync(path).map(name => join(path, name)).filter(isFile).map(pathConverter)
 }
+
+function removeTemplate(projectName, templateName) {
+  return unlinkSync(`${__dirname}/${projectName}/${templateName}`);
+}
+
+function removeProject(projectName) {
+  return deleteFolderRecursive(`${__dirname}/templates/${projectName}`);
+}
+
 module.exports = {
   listProjects,
-  listTemplates
+  listTemplates,
+  removeTemplate,
+  removeProject
 };
